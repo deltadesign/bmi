834af43f739e92aed1a2bdfb7dd1299ed3335b74
unknown <alexkynoch@yahoo.co.uk>
2021-05-07 19:07:52 +0100

    new form added

--- app.js
+++ app.js
@@ -18,3 +18,17 @@ function bmi(e) {
   result.innerHTML = (((w / h) * 100).toFixed(2));
 }
 
+function openMeasure(evt, unitType) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(unitType).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
