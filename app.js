f5c3f1840eb5bef46297e4bcea1808727a68297b
Dan Thomas <66531548+deltadesign@users.noreply.github.com>
2021-05-12 19:07:49 +0100

    Messages added to result

--- app.js
+++ app.js
@@ -14,7 +14,18 @@ function bmi(e) {
 
   form.style.display = 'none';
 
+  let bmi = parseFloat(((w / h) * 100).toFixed(2));
+
+  if (bmi < 18.5){
+    result.innerHTML = `A BMI of ${bmi} is considered underweight`;
+  } else if (bmi > 25) {
+    result.innerHTML = `A BMI of ${bmi} is considered overweight`
+  } else {
+    result.innerHTML = `A BMI of ${bmi} is considered healthy`
+  }
+
+
   result.style.display = 'block';
-  result.innerHTML = (((w / h) * 100).toFixed(2));
+  // result.innerHTML = (((w / h) * 100).toFixed(2));
 }
 
